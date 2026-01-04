eaffccfb1e184f5ac4849d32d431d56a57a837d6
butizinhaff24-hash <butizinhaff24@gmail.com>
2026-01-04 19:01:03 +0100

    Update app.js.js

--- app.js.js
+++ app.js.js
@@ -1,1 +1,11 @@
-app.js
+app.jsconst express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Sistema de Pagamento Angola</h1><p>O servidor está online e funcional!</p>');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor a rodar na porta ${port}`);
+});
